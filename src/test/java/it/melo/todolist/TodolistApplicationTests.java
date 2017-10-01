3645ed24b0a3c6588c303cc46a43a07e8e677cb2
Carmelo Foti <carmelo.foti86@gmail.com>
2017-10-01 18:46:40 +0200

    added basic test

--- src/test/java/it/melo/todolist/TodolistApplicationTests.java
+++ src/test/java/it/melo/todolist/TodolistApplicationTests.java
@@ -1,16 +1,41 @@
 package it.melo.todolist;
 
+import it.melo.todolist.entities.Todo;
+import it.melo.todolist.repositories.TodoRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TodolistApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	TodoRepository repository;
 
+	@Test
+	@Transactional
+	public void contextLoads() {
+		Todo todo1 = new Todo();
+		Todo todo2 = new Todo();
+		todo1.setId(1);
+		todo1.setText("todo 1");
+		todo2.setId(2);
+		todo2.setText("todo 2");
+		repository.save(todo1);
+		repository.save(todo2);
+		assertThat(repository.count()).isEqualTo(2);
+		String MESSAGE = "new message";
+		todo2.setText(MESSAGE);
+		repository.save(todo2);
+		assertThat(repository.count()).isEqualTo(2);
+		Todo persistedTodo = repository.getOne(2L);
+		assertThat(persistedTodo.getText()).isEqualTo(MESSAGE);
+	}
 }
